aba5310d159bae14ec8f94da24efb36f6529682b
geniuspopeye <sasha.batalin@gmail.com>
2020-12-15 21:14:47 +0300

    Update GameField.cs

--- CourseWork2020/GameField.cs
+++ CourseWork2020/GameField.cs
@@ -18,17 +18,27 @@ namespace CourseWork2020
                 }
             }
         }
-        public void check()//сравнивает поля ориджинал и проблем
+        public bool check()//сравнивает поля ориджинал и проблем
         {
-
+            for(int i=0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (originalField[i, j] == problemField[i, j])
+                        continue;
+                    else
+                        return false;
+                }
+            }
+            return true;
         }
         public void add()//добавляет клетку в problem
         {
 
         }
-        public void delete()//удаляет клетку из problem
+        public void delete(int linePos, int colPos)//удаляет клетку из problem
         {
-
+            problemField[linePos, colPos] = 0;
         }
         private void shaffle()//перемешивает поле с помощью нижних функций
         {
@@ -36,7 +46,15 @@ namespace CourseWork2020
         }
         private void transpos()//транспонирует матрицу
         {
-
+            int[,] tempArray = new int[9, 9];
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    tempArray[i, j] = originalField[j, i];
+                }
+            }
+            originalField = tempArray;
         }
         private void swapLine()//свапает одну линию с другой
         {
@@ -44,7 +62,25 @@ namespace CourseWork2020
         }
         private void swapBigLine()//свапает большую линию из 3 с другой 
         {
-
+            int n = 3;
+            Random l = new Random();
+            var block1 = l.Next(0, n);
+            var block2 = l.Next(0, n);
+            while (block1 == block2)
+                block2 = l.Next(0, n);
+            block1 *= n;
+            block2 *= n;
+            for(int i =0; i < n * n; i++)
+            {
+                var t = block2;
+                for(int j = block1; j < block1 + n; j++)
+                {
+                    var k = originalField[j, i];
+                    originalField[j, i] = originalField[k, i];
+                    originalField[j, i] = k;
+                    t++;
+                }
+            }
         }
         private void swapColumne()//свапает одну колонку с другой
         {
@@ -52,7 +88,9 @@ namespace CourseWork2020
         }
         private void swapBigColumne()//свапает большую колонку из 3 с другой 
         {
-
+            transpos();
+            swapBigLine();
+            transpos();
         }
     }
 }
