ea22f00388f38030cf322c68e5f32e22fe5c1cde
atrevalis <atrevalis@mail.ru>
2020-12-15 21:06:44 +0300

    d

--- CourseWork2020/GameField.cs
+++ CourseWork2020/GameField.cs
@@ -5,7 +5,7 @@ namespace CourseWork2020
     {
         int[,] originalField;//поле ответов
         int[,] startField;//поле изначального состояния НЕ МЕНЯТЬ
-        int[,] problemField;//поле основной игры изсменяемой игроком 
+        int[,] problemField;//поле основной игры изменяемой игроком 
 
         public GameField()//составляет изначальную сетку поля
         {
@@ -17,14 +17,22 @@ namespace CourseWork2020
                     originalField[i,j]=(j+i*3)%9;
                 }
             }
+            shaffle();
         }
         public void check()//сравнивает поля ориджинал и проблем
         {
 
         }
-        public void add()//добавляет клетку в problem
+        public void add(int num,int x,int y)//добавляет клетку в problem
         {
-
+            if (startField[x,y]==0)
+            {
+                problemField[x, y] = num;
+            }
+            else
+            {
+                //здесь возможно будет вызов всплывающего окошка
+            }
         }
         public void delete()//удаляет клетку из problem
         {
@@ -32,7 +40,32 @@ namespace CourseWork2020
         }
         private void shaffle()//перемешивает поле с помощью нижних функций
         {
-
+            Random random = new Random();
+            int rand = random.Next(10, 101);//уменьшить при долгом создании матрицы(на данный момент 9<х<101)
+            for (int i = 0;i<=rand;i++)
+            {
+                switch(random.Next(1, 6))
+                {
+                    case 1:
+                        this.transpos();
+                        break;
+                    case 2:
+                        this.swapLine();
+                        break;
+                    case 3:
+                        this.swapBigLine();
+                        break;
+                    case 4:
+                        this.swapColumne();
+                        break;
+                    case 5:
+                        this.swapBigColumne();
+                        break;
+                    default:
+                        this.transpos();//на всякий
+                        break;
+                }
+            }
         }
         private void transpos()//транспонирует матрицу
         {
@@ -40,6 +73,13 @@ namespace CourseWork2020
         }
         private void swapLine()//свапает одну линию с другой
         {
+            Random random = new Random();
+            int first = random.Next(9);
+            int second = random.Next(2);
+            int[] swLine = new int[9];
+            for (int i = 0;i<9 ;i++) {
+
+            }
 
         }
         private void swapBigLine()//свапает большую линию из 3 с другой 
