32fa1c5772f7eced3bc42339a23373fbf8c0d1f1
Kumarazavr <62707618+Kumarazavr@users.noreply.github.com>
2020-12-19 16:07:08 +0300

    Add files via upload

---
+++ posos3/posos3/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace posos3
+{
+    public partial class Form1 : Form
+    {
+        class SudokuCell : Button
+        {
+            public int Value { get; set; }
+            public bool IsLocked { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public void Clear()
+            {
+                this.Text = string.Empty;
+                this.IsLocked = false;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = "beginner";
+            comboBox2.SelectedItem = "standart";
+        }
+        SudokuCell[,] cells = new SudokuCell[9, 9];
+
+        private void createCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    cells[i, j] = new SudokuCell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.AliceBlue;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Black;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    cells[i, j].KeyPress += cell_keyPressed;
+
+                    panel2.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as SudokuCell;
+
+            if (cell.IsLocked)
+                return;
+
+            int value;
+
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+    
+
+    private void NewGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            createCells();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }    
+
+}
